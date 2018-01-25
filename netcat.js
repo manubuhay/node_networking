859de5b1f8462bfa9792a3e14008e21c480289d0
manu buhay <manubuhay@gmail.com>
2018-01-26 00:16:41 +0800

    added exit function to netcat.js

--- netcat.js
+++ netcat.js
@@ -1,9 +1,9 @@
 var nc = require("node-cat");
  
-var client = nc.createClient("ec2-34-197-72-10.compute-1.amazonaws.com", "9091");
+var client = nc.createClient("ec2-34-197-72-10.compute-1.amazonaws.com", "9090");
  
 client.start(
 function(client /* the tcp client returned by net.connect */, rl /* readline instance */, stdin, stdout){
     client.write("STATUS: None of your business");
-    stdout.write("Sent status.");
+    stdout.write("Sent status.");process.exit(0);
 });
