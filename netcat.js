0e33ff576d8a42a50ad7848b0c03f7d10664ba13
Manu Buhay <manubuhay@gmail.com>
2018-01-25 08:22:59 -0800

    added new line to netcat.js after exit function

--- netcat.js
+++ netcat.js
@@ -5,5 +5,5 @@ var client = nc.createClient("ec2-34-197-72-10.compute-1.amazonaws.com", "9090")
 client.start(
 function(client /* the tcp client returned by net.connect */, rl /* readline instance */, stdin, stdout){
     client.write("STATUS: None of your business");
-    stdout.write("Sent status.");process.exit(0);
+    stdout.write("Sent status.\n");process.exit(0);
 });
