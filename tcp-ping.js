b0db7778229f085499503d78f50b91761aca0cc3
manu buhay <manubuhay@gmail.com>
2018-01-25 13:14:45 +0800

    edited tcp-ping.js

--- tcp-ping.js
+++ tcp-ping.js
@@ -4,14 +4,17 @@ const axios = require('axios');
 function notify(){
 axios.post("https://hooks.glip.com/webhook/ad559b30-c392-468d-a0c5-ec8d808a95fc", {
   "icon": "https://vignette.wikia.nocookie.net/logopedia/images/2/2c/Lazada_Icon.png",
-  "activity": "Shem",
-  "title": "Test Hook",
+  "activity": "TCP-PINGHook",
+  "title": "APNS Server IS DOWN",
   //"body": "* Location: [The Funky Buddha Lounge](http://www.thefunkybuddha.com)\n*Beer Advocate Rating: [99](http://tinyurl.com/psf4uzq)"
 });
 }
 
-tcpp.probe('ec2-34-197-72-10.compute-1.amazonaws.com', 9090, function(err, available) {
-    console.log(available);notify();
+tcpp.probe('ec2-34-197-72-10.compute-1.amazonaws.com', 9091, function(err, available) {
+    console.log(available);//notify();
+    if(available == "false"){
+    	notify();
+    }
 });
  
 // tcpp.ping({ address: 'ec2-34-197-72-10.compute-1.amazonaws.com' }, function(err, data) {
