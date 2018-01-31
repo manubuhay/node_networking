85fa54e29e117c19ee5896c0a7a9dedee8c7331d
manu buhay <manubuhay@gmail.com>
2018-01-31 08:51:31 +0800

    edited tcp-ping-arrays.js file

--- tcp-ping-array.js
+++ tcp-ping-array.js
@@ -18,33 +18,40 @@ axios.post("https://hooks.glip.com/webhook/e8c7f15c-ceb7-474f-a42a-560f70a036a7"
 // 	process.exit(0);
 // });
 
-tcpp.probe('ec2-54-165-245-68.compute-1.amazonaws.com', 4000, function(err, available) {
-    console.log(available);
-    if(available == false)
-    notify();
-	// else
-	// process.exit(0);
-});
+// tcpp.probe('ec2-54-165-245-68.compute-1.amazonaws.com', 4000, function(err, available) {
+//     console.log(available);
+//     if(available == false)
+//     notify();
+// 	// else
+// 	// process.exit(0);
+// });
 
-tcpp.probe('ec2-52-7-161-56.compute-1.amazonaws.com', 8080, function(err, available) {
-    console.log(available);
-    if(available == false)
-    notify();
-	// else
-	// process.exit(0);
-});
+// tcpp.probe('ec2-52-7-161-56.compute-1.amazonaws.com', 8080, function(err, available) {
+//     console.log(available);
+//     if(available == false)
+//     notify();
+// 	// else
+// 	// process.exit(0);
+// });
 
-tcpp.probe('ec2-52-20-35-81.compute-1.amazonaws.com', 8080, function(err, available) {
-    console.log(available);
-    if(available == false)
-    notify();
-	// else
-	// process.exit(0);
-});
+// tcpp.probe('ec2-52-20-35-81.compute-1.amazonaws.com', 8080, function(err, available) {
+//     console.log(available);
+//     if(available == false)
+//     notify();
+// 	// else
+// 	// process.exit(0);
+// });
  
  // This part of the code takes description from the servers
 // tcpp.ping({ address: 'ec2-34-197-72-10.compute-1.amazonaws.com' }, function(err, data) {
 //     console.log(data); 
 // });
 
-//var server ["{server1: "ec2-34-197-72-10.compute-1.amazonaws.com", port1: "9090"}", ]
+var server = [{server1: "ec2-34-197-72-10.compute-1.amazonaws.com", port1: "9090"},
+				{server2: "ec2-54-165-245-68.compute-1.amazonaws.com", port2: "4000"}, 
+				{server3: "ec2-52-7-161-56.compute-1.amazonaws.com", port3: "8080"},
+				{server4: "ec2-52-20-35-81.compute-1.amazonaws.com", port4: "8080"} ]
+ 
+console.log(server[0].server1);
+console.log(server[1].port2);
+console.log(server[2].port3);
