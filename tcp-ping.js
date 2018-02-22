f497fcff6c995a8ff808c5813c1e9295c9a19b97
manubuhay <manubuhay@gmail.com>
2018-02-22 15:54:50 +0800

    Update tcp-ping.js

--- tcp-ping.js
+++ tcp-ping.js
@@ -3,14 +3,14 @@ const axios = require('axios');
 
 //Creates POST request
 function notify(){
-axios.post("https://hooks.glip.com/webhook/005dbe1d-7eed-4316-8879-8836042f6cc8", {
+axios.post("https://hooks.glip.com/webhook/fee0afc9-7566-4730-98d0-084c7c6055d8", {
   "icon": "http://www.qmsacademy.com/wp-content/uploads/2015/10/DevOps-logo.png",
   "activity": "TCP-PING-Hook",
-  "title": "MANU GWAPO",
+  "title": "APNS DOWN!",
 });
 }
 
-tcpp.probe('10.1.55.254', 8009, function(err, available) {
+tcpp.probe('ec2-34-197-72-10.compute-1.amazonaws.com', 9091, function(err, available) {
     console.log(available);
     if(available == false)
     notify();
@@ -22,4 +22,3 @@ tcpp.probe('10.1.55.254', 8009, function(err, available) {
 // tcpp.ping({ address: 'ec2-34-197-72-10.compute-1.amazonaws.com' }, function(err, data) {
 //     console.log(data); 
 // });
-
